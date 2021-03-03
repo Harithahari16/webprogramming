ce77aef2ff7a50c0967a02b65d34d051ed7fe930
Student <Student@mcalab.edu>
2021-03-03 16:01:51 +0530

    second commit

---
+++ web1/13.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+    <title>Sorting Indexed Array in Ascending Order</title>
+</head>
+<body>
+
+<?php
+
+$names = array("Haritha", "Gopika", "Biya", "Riya");
+ 
+
+arsort($names);
+
+print_r($names);
+asort($names);
+print_r($names);
+?>
+
+</body>
+</html>
